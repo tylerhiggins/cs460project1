d5a89609988f7a701943a86c57cad39b9c7a8fc5
Tyler Higgins <tyler.higgins@wsu.edu>
2020-01-13 12:52:46 -0800

    Renamed my_cat to my-cat
    Possibly finished with my-cat.c, going to make sure there are no details missed before confirming finished.

--- my-cat.c
+++ my-cat.c
@@ -6,9 +6,10 @@
  * std out.
 */
 #include<stdio.h>
-int main(int argc, char[]argv){
-	char buffer[];          // Buffer in which to store characters.
-	FILE file = NULL;       // Pointer to the file.
+int main(int argc, char *argv[]){
+	int buf = 256;			// Buffer size. 
+	char buffer[buf];       // Buffer in which to store characters.
+	FILE *file = NULL;       // Pointer to the file.
 	/* Iterates through each filename given in the arguments
 	   ignoring index 0 since argv[0] is the name of the program
 	   itself. */
@@ -19,8 +20,13 @@ int main(int argc, char[]argv){
 			printf("my_cat: Could not open file %s\n", argv[i]);
 			return 1;
 		}
+		/* Read the contents of the file. So long as fgets does not
+		   read EOF. */
+		while(fgets(buffer,buf,file) != NULL){
+			printf("%s",buffer);
+		}
 
-		file.close();
+		fclose(file);
 	}
 	return 0;
 }
